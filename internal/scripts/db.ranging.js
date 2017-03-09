dd2201187bc09ca9f7425b7b76e15a469fb6270c
Jacob Phillips <jifalops@gmail.com>
2017-03-09 14:16:18 -0500

    Commented out incorrect scripts

--- internal/scripts/db.ranging.js
+++ internal/scripts/db.ranging.js
@@ -1,57 +1,57 @@
 var db = db || {};
 
 db.ranging = {
-  activateRssWifi4gSettings: function(user, settings, onSuccess, onFailure) {
-    DB && console.log('Activating settings for WiFi 4G RSS');
-    var key = db.newKey('/nnRssWifi4g');
-    var updates = db.userActionUpdate(user);
-    updates['/nnRssWifi4g/' + key] = settings;
-    updates['/activeNnRssWifi4g'] = settings;
-    db.update(updates, onSuccess, onFailure);
-  },
-
-  activateRssWifi5gSettings: function(user, settings, onSuccess, onFailure) {
-    DB && console.log('Activating settings for WiFi 5G RSS');
-    var key = db.newKey('/nnRssWifi5g');
-    var updates = db.userActionUpdate(user);
-    updates['/nnRssWifi5g/' + key] = settings;
-    updates['/activeNnRssWifi5g'] = settings;
-    db.update(updates, onSuccess, onFailure);
-  },
-
-  activateRssBtSettings: function(user, settings, onSuccess, onFailure) {
-    DB && console.log('Activating settings for Bt RSS');
-    var key = db.newKey('/nnRssBt');
-    var updates = db.userActionUpdate(user);
-    updates['/nnRssBt/' + key] = settings;
-    updates['/activeNnRssBt'] = settings;
-    db.update(updates, onSuccess, onFailure);
-  },
-
-  activateRssBtleSettings: function(user, settings, onSuccess, onFailure) {
-    DB && console.log('Activating settings for Btle RSS');
-    var key = db.newKey('/nnRssBtle');
-    var updates = db.userActionUpdate(user);
-    updates['/nnRssBtle/' + key] = settings;
-    updates['/activeNnRssBtle'] = settings;
-    db.update(updates, onSuccess, onFailure);
-  },
-
-  activateTofBtHciSettings: function(user, settings, onSuccess, onFailure) {
-    DB && console.log('Activating settings for Bt HCI TOF');
-    var key = db.newKey('/nnTofBtHci');
-    var updates = db.userActionUpdate(user);
-    updates['/nnTofBtHci/' + key] = settings;
-    updates['/activeNnTofBtHci'] = settings;
-    db.update(updates, onSuccess, onFailure);
-  },
-
-  activateTofBtJavaSettings: function(user, settings, onSuccess, onFailure) {
-    DB && console.log('Activating settings for Bt Java TOF');
-    var key = db.newKey('/nnTofBtJava');
-    var updates = db.userActionUpdate(user);
-    updates['/nnTofBtJava/' + key] = settings;
-    updates['/activeNnTofBtJava'] = settings;
-    db.update(updates, onSuccess, onFailure);
-  }
+  // activateRssWifi4gSettings: function(user, settings, onSuccess, onFailure) {
+  //   DB && console.log('Activating settings for WiFi 4G RSS');
+  //   var key = db.newKey('/nnRssWifi4g');
+  //   var updates = db.userActionUpdate(user);
+  //   updates['/nnRssWifi4g/' + key] = settings;
+  //   updates['/activeNnRssWifi4g'] = settings;
+  //   db.update(updates, onSuccess, onFailure);
+  // },
+  //
+  // activateRssWifi5gSettings: function(user, settings, onSuccess, onFailure) {
+  //   DB && console.log('Activating settings for WiFi 5G RSS');
+  //   var key = db.newKey('/nnRssWifi5g');
+  //   var updates = db.userActionUpdate(user);
+  //   updates['/nnRssWifi5g/' + key] = settings;
+  //   updates['/activeNnRssWifi5g'] = settings;
+  //   db.update(updates, onSuccess, onFailure);
+  // },
+  //
+  // activateRssBtSettings: function(user, settings, onSuccess, onFailure) {
+  //   DB && console.log('Activating settings for Bt RSS');
+  //   var key = db.newKey('/nnRssBt');
+  //   var updates = db.userActionUpdate(user);
+  //   updates['/nnRssBt/' + key] = settings;
+  //   updates['/activeNnRssBt'] = settings;
+  //   db.update(updates, onSuccess, onFailure);
+  // },
+  //
+  // activateRssBtleSettings: function(user, settings, onSuccess, onFailure) {
+  //   DB && console.log('Activating settings for Btle RSS');
+  //   var key = db.newKey('/nnRssBtle');
+  //   var updates = db.userActionUpdate(user);
+  //   updates['/nnRssBtle/' + key] = settings;
+  //   updates['/activeNnRssBtle'] = settings;
+  //   db.update(updates, onSuccess, onFailure);
+  // },
+  //
+  // activateTofBtHciSettings: function(user, settings, onSuccess, onFailure) {
+  //   DB && console.log('Activating settings for Bt HCI TOF');
+  //   var key = db.newKey('/nnTofBtHci');
+  //   var updates = db.userActionUpdate(user);
+  //   updates['/nnTofBtHci/' + key] = settings;
+  //   updates['/activeNnTofBtHci'] = settings;
+  //   db.update(updates, onSuccess, onFailure);
+  // },
+  //
+  // activateTofBtJavaSettings: function(user, settings, onSuccess, onFailure) {
+  //   DB && console.log('Activating settings for Bt Java TOF');
+  //   var key = db.newKey('/nnTofBtJava');
+  //   var updates = db.userActionUpdate(user);
+  //   updates['/nnTofBtJava/' + key] = settings;
+  //   updates['/activeNnTofBtJava'] = settings;
+  //   db.update(updates, onSuccess, onFailure);
+  // }
 };
