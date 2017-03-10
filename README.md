c5ff8766a7fb50f8be0965be72b7d3c1ea351734
jifalops <jifalops@gmail.com>
2017-03-10 00:15:23 -0500

    Successfully created ranging params and activated them

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ Web interface for the [Android app](https://github.com/jifalops/localization).
 
 
 Try it at https://localization-web-7535f.firebaseapp.com
+
+## Purpose
+View data submitted by the companion app and specify certain app settings.
+
+1. View raw RSS/TOF samples.
+2. Create settings for the app that specify how to perform ranging.
+3. Keep each version of the settings and allow you to choose which ones are
+   active in the app.
+4. View ranging results corresponding to each combination of settings.
--- internal/scripts/db.ranging.js
+++ internal/scripts/db.ranging.js
@@ -1,57 +1,19 @@
 var db = db || {};
 
 db.ranging = {
-  // activateRssWifi4gSettings: function(user, settings, onSuccess, onFailure) {
-  //   DB && console.log('Activating settings for WiFi 4G RSS');
-  //   var key = db.newKey('/nnRssWifi4g');
-  //   var updates = db.userActionUpdate(user);
-  //   updates['/nnRssWifi4g/' + key] = settings;
-  //   updates['/activeNnRssWifi4g'] = settings;
-  //   db.update(updates, onSuccess, onFailure);
-  // },
-  //
-  // activateRssWifi5gSettings: function(user, settings, onSuccess, onFailure) {
-  //   DB && console.log('Activating settings for WiFi 5G RSS');
-  //   var key = db.newKey('/nnRssWifi5g');
-  //   var updates = db.userActionUpdate(user);
-  //   updates['/nnRssWifi5g/' + key] = settings;
-  //   updates['/activeNnRssWifi5g'] = settings;
-  //   db.update(updates, onSuccess, onFailure);
-  // },
-  //
-  // activateRssBtSettings: function(user, settings, onSuccess, onFailure) {
-  //   DB && console.log('Activating settings for Bt RSS');
-  //   var key = db.newKey('/nnRssBt');
-  //   var updates = db.userActionUpdate(user);
-  //   updates['/nnRssBt/' + key] = settings;
-  //   updates['/activeNnRssBt'] = settings;
-  //   db.update(updates, onSuccess, onFailure);
-  // },
-  //
-  // activateRssBtleSettings: function(user, settings, onSuccess, onFailure) {
-  //   DB && console.log('Activating settings for Btle RSS');
-  //   var key = db.newKey('/nnRssBtle');
-  //   var updates = db.userActionUpdate(user);
-  //   updates['/nnRssBtle/' + key] = settings;
-  //   updates['/activeNnRssBtle'] = settings;
-  //   db.update(updates, onSuccess, onFailure);
-  // },
-  //
-  // activateTofBtHciSettings: function(user, settings, onSuccess, onFailure) {
-  //   DB && console.log('Activating settings for Bt HCI TOF');
-  //   var key = db.newKey('/nnTofBtHci');
-  //   var updates = db.userActionUpdate(user);
-  //   updates['/nnTofBtHci/' + key] = settings;
-  //   updates['/activeNnTofBtHci'] = settings;
-  //   db.update(updates, onSuccess, onFailure);
-  // },
-  //
-  // activateTofBtJavaSettings: function(user, settings, onSuccess, onFailure) {
-  //   DB && console.log('Activating settings for Bt Java TOF');
-  //   var key = db.newKey('/nnTofBtJava');
-  //   var updates = db.userActionUpdate(user);
-  //   updates['/nnTofBtJava/' + key] = settings;
-  //   updates['/activeNnTofBtJava'] = settings;
-  //   db.update(updates, onSuccess, onFailure);
-  // }
+  createSettings: function(user, type, settings, onSuccess, onFailure) {
+    DB && console.log('Creating settings for ' + type);
+    var key = db.newKey('/' + type);
+    var updates = db.userActionUpdate(user);
+    updates['/' + type + '/' + key] = settings;
+    db.update(updates, onSuccess, onFailure);
+    return key;
+  },
+  
+  activateSettings: function(user, type, key, onSuccess, onFailure) {
+    DB && console.log('Activating settings for', type, key);
+    var updates = db.userActionUpdate(user);
+    updates['/' + type] = key;
+    db.update(updates, onSuccess, onFailure);
+  }
 };
